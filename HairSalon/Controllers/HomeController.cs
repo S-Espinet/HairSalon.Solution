53fbc3b713c4d30ff7cc8e7229ddb121df9934bd
S. Espinet <s.lou.espinet@gmail.com>
2022-03-11 09:09:22 -0800

    add code to Home files

---
+++ HairSalon/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace HairSalon.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly HairSalonContext _db;
+    public HomeController(HairSalonContext db)
+    {
+      _db = db;
+    }
+  
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylists.ToList();
+      return View(model);
+    }
+  }
+}
